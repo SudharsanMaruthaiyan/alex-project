5ee060e4d413648e6e7f6d0233a98fe5a87d71b0
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2025-02-07 00:32:53 +0530

    make card for courses

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,80 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <p className=' text-center text-4xl font-bold'>Welcome to MERN stack</p>
+      <p className=' text-center text-4xl font-bold py-3'>Welcome to MERN stack</p>
       <div>
-        
+        <div className=' grid grid-cols-4 px-[100px] pt-10 gap-10'>
+            <div className=' shadow-2xl  p-2 rounded-xl hover:scale-105 transition-all'>
+                <div>
+                    <img src="https://ik.imagekit.io/jjyo3gsee/RevampImg/course1.png?updatedAt=1730215622138" alt="" />
+                        <img src="" alt="" />
+                </div>
+                <div>
+                    <p className=' text-2xl font-bold pt-2'>Mern Stack</p>
+                    <p className=' text-base text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, tempora.</p>
+                    <div className=' flex items-center gap-3 py-5'>
+                        <button className=' bg-black text-white hover:bg-white hover:border-black hover:border hover:text-black py-3 px-6 rounded-lg'>Start Now</button>
+                        <button className=' bg-blue-500 text-white hover:bg-white hover:border hover:text-black py-3 px-6 rounded-lg'>$5000</button>
+                    </div>
+                </div>
+            </div>
+            <div className=' shadow-2xl  p-2 rounded-xl hover:scale-105 transition-all'>
+                <div>
+                    <img src="https://ik.imagekit.io/jjyo3gsee/RevampImg/course1.png?updatedAt=1730215622138" alt="" />
+                        <img src="" alt="" />
+                </div>
+                <div>
+                    <p className=' text-2xl font-bold pt-2'>Mern Stack</p>
+                    <p className=' text-base text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, tempora.</p>
+                    <div className=' flex items-center gap-3 py-5'>
+                        <button className=' bg-black text-white hover:bg-white hover:border-black hover:border hover:text-black py-3 px-6 rounded-lg'>Start Now</button>
+                        <button className=' bg-blue-500 text-white hover:bg-white hover:border hover:text-black py-3 px-6 rounded-lg'>$5000</button>
+                    </div>
+                </div>
+            </div>
+            <div className=' shadow-2xl  p-2 rounded-xl hover:scale-105 transition-all'>
+                <div>
+                    <img src="https://ik.imagekit.io/jjyo3gsee/RevampImg/course1.png?updatedAt=1730215622138" alt="" />
+                        <img src="" alt="" />
+                </div>
+                <div>
+                    <p className=' text-2xl font-bold pt-2'>Mern Stack</p>
+                    <p className=' text-base text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, tempora.</p>
+                    <div className=' flex items-center gap-3 py-5'>
+                        <button className=' bg-black text-white hover:bg-white hover:border-black hover:border hover:text-black py-3 px-6 rounded-lg'>Start Now</button>
+                        <button className=' bg-blue-500 text-white hover:bg-white hover:border hover:text-black py-3 px-6 rounded-lg'>$5000</button>
+                    </div>
+                </div>
+            </div>
+            <div className=' shadow-2xl  p-2 rounded-xl hover:scale-105 transition-all'>
+                <div>
+                    <img src="https://ik.imagekit.io/jjyo3gsee/RevampImg/course1.png?updatedAt=1730215622138" alt="" />
+                        <img src="" alt="" />
+                </div>
+                <div>
+                    <p className=' text-2xl font-bold pt-2'>Mern Stack</p>
+                    <p className=' text-base text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, tempora.</p>
+                    <div className=' flex items-center gap-3 py-5'>
+                        <button className=' bg-black text-white hover:bg-white hover:border-black hover:border hover:text-black py-3 px-6 rounded-lg'>Start Now</button>
+                        <button className=' bg-blue-500 text-white hover:bg-white hover:border hover:text-black py-3 px-6 rounded-lg'>$5000</button>
+                    </div>
+                </div>
+            </div>
+            <div className=' shadow-2xl  p-2 rounded-xl hover:scale-105 transition-all'>
+                <div>
+                    <img src="https://ik.imagekit.io/jjyo3gsee/RevampImg/course1.png?updatedAt=1730215622138" alt="" />
+                        <img src="" alt="" />
+                </div>
+                <div>
+                    <p className=' text-2xl font-bold pt-2'>Mern Stack</p>
+                    <p className=' text-base text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, tempora.</p>
+                    <div className=' flex items-center gap-3 py-5'>
+                        <button className=' bg-black text-white hover:bg-white hover:border-black hover:border hover:text-black py-3 px-6 rounded-lg'>Start Now</button>
+                        <button className=' bg-blue-500 text-white hover:bg-white hover:border hover:text-black py-3 px-6 rounded-lg'>$5000</button>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   )
