7739e3e997daa92c2ae7d4bd2a5e53ec10167b09
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2025-02-07 00:18:21 +0530

    deploy demo project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className=' text-center text-4xl font-bold'>Welcome to MERN stack</p>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
